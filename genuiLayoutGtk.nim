d848f6c882e8504f81e7b6e67ccf98be73297447
Peter Munch-Ellingsen <peterme@peterme.net>
2017-10-12 22:04:13 +0200

    Started looking at how the layout should be generated

--- genuiLayoutGtk.nim
+++ genuiLayoutGtk.nim
@@ -4,7 +4,20 @@ from oldgtk3/gobject import nil
 from oldgtk3/glib import nil
 
 macro createLayout(layout: static[UILayout]): untyped =
-  result = nemStmtList()
+  result = newStmtList()
+  macro generateLayer(oldSym: NimNode, layout: static[UIContainer]): untyped =
+    result = newStmtList()
+    var sym: NimNode
+    case layout.kind:
+      of Row, Widget:
+        sym = oldSym
+      of Column:
+        sym = genSym(nskVar)
+        result.add(quote do:
+          `sym` = gtk.newGrid()
+          gtk.setColumnHomogeneous(`sym`, true)
+        )
+
   let base = genSym(nskVar)
   result.add(quote do:
     `base` = gtk.newBox(gtk.Orientation.VERTICAL, 5)
